0b7cd8bdc6ab295f0f4161400a0fb0bf3fd4a8f2
Sadness <rosalimavictor@gmail.com>
2024-08-20 10:09:35 -0300

    Centralized tile

--- src/main/java/codes/shiftmc/hrm/Server/Game.java
+++ src/main/java/codes/shiftmc/hrm/Server/Game.java
@@ -9,6 +9,7 @@ import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.world.DimensionType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -20,6 +21,7 @@ public class Game {
     private final Instance instance;
 
     private final List<Short> output = List.of();
+    private final ArrayList<Tile> tiles = new ArrayList<>();
 
     public Game(short[] ram, short[] expected, short[] input) {
         this.ram = ram;
@@ -29,16 +31,7 @@ public class Game {
         InstanceContainer instance = MinecraftServer.getInstanceManager().createInstanceContainer(DimensionType.OVERWORLD);
         instance.setChunkSupplier(LightingChunk::new);
 
-        // chess board platform 14 x 18
-        List<Block> blocks = List.of(Block.STRIPPED_SPRUCE_WOOD, Block.STRIPPED_OAK_WOOD);
-        for (int x = 0; x < 14; x++) {
-            for (int z = 0; z < 18; z++) {
-                instance.setBlock(x, 0, z, blocks.get((x + z) % 2));
-            }
-        }
-
         this.instance = instance;
-
         init();
     }
 
@@ -58,9 +51,15 @@ public class Game {
                 instance.setBlock(x, 42, z, blocks.get((x + z) % 2));
             }
         }
+
+        var start = new Pos(4, 44, 2.5);
+        for (short i : input) {
+            start = new Pos(start.x() + 0.6, start.y(), start.z());
+            tiles.add(new Tile(instance, start));
+        }
     }
 
     public void start(Player player) {
-        player.setInstance(instance, new Pos(0, 42, 0));
+        player.setInstance(instance, new Pos(7, 43, 9));
     }
 }
--- src/main/java/codes/shiftmc/hrm/Server/Tile.java
+++ src/main/java/codes/shiftmc/hrm/Server/Tile.java
@@ -1,25 +1,26 @@
 package codes.shiftmc.hrm.Server;
 
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.EntityCreature;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.metadata.display.BlockDisplayMeta;
 import net.minestom.server.entity.metadata.display.ItemDisplayMeta;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 
-public class Tile extends EntityCreature {
+public class Tile extends Entity {
     public Tile(Instance instance, Pos position) {
         super(EntityType.ITEM_DISPLAY);
         var meta = (ItemDisplayMeta) getEntityMeta();
 
         meta.setNotifyAboutChanges(false);
-        meta.setHeight(0.5f);
-        meta.setWidth(0.5f);
+        meta.setScale(new Vec(0.5f, 0.5f, 0.5f));
         meta.setItemStack(ItemStack.of(Material.GREEN_WOOL));
         meta.setNotifyAboutChanges(true);
 
         setInstance(instance, position);
     }
-
 }
